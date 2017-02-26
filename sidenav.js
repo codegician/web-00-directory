34c9b470b039392ac62ab6bbe8ef5cb685424bc0
ewwjackson <ewartjackson@icloud.com>
2017-02-26 07:55:43 -0800

    nav bar

---
+++ sidenav.js
@@ -0,0 +1,11 @@
+function openNav() {
+    document.getElementById("mysidenav").style.width = "230px";
+    // document.getElementById("main").style.marginLeft = "250px";
+    document.body.style.backgroundColor = "rgba(0,0,0,0.1)";
+}
+
+function closeNav() {
+    document.getElementById("mysidenav").style.width = "0";
+    // document.getElementById("main").style.marginLeft = "0";
+    document.body.style.backgroundColor = "white";
+}
